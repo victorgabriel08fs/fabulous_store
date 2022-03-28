aa810b9f76f304ffc1092368e37ffa4ee47403fa
Victor Gabriel <victorgabriel08fs@gmail.com>
2022-03-28 15:49:58 -0300

    Iniciando parte do administrador

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductSeeder::class);
         $this->call(OrderSeeder::class);
         $this->call(CouponSeeder::class);
+        $this->call(AdminSeeder::class);
     }
 }
