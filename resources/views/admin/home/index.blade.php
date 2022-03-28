83581ae286aec88107ea1a2113691678625cfc67
Victor Gabriel <victorgabriel08fs@gmail.com>
2022-03-28 17:52:39 -0300

    Teste CSS

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.app')
 @section('content')
-    <p>aaaaaaaaaaaaaaa</p>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Dashboard
+                    </div>
+                    <div class="card-body">
+                        <div class="dashboard">
+                            <div class="dashboard-item item-1">
+                                <p>1</p>
+                            </div>
+                            <div class="dashboard-item item-2">
+                                <p>2</p>
+                            </div>
+                            <div class="dashboard-item item-3">
+                                <p>3</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
